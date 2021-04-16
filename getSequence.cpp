da44f442f0144fc651ac848604582f04d4327266
YeefyisOK <wyf_5590@163.com>
2021-04-16 15:17:59 +0800

    option merge -sequence8

--- getSequence.cpp
+++ getSequence.cpp
@@ -66,6 +66,10 @@ vector<string> AOIs{
 vector<string> AOIs2{
 	"Diagram","OptionA","OptionB","OptionC","OptionD","Statement","Time","Else"
 };
+
+vector<string> AOIs3{
+	"Diagram","Option","Statement","Time","Else"
+};
 int inWhichAOI(int x,int y) {
 	if (x >= diagram_LU.x&&x <= diagram_RB.x &&y >= diagram_LU.y&&y <= diagram_RB.y) {
 		return 0;
@@ -119,6 +123,23 @@ int inWhichAOI2(int x, int y) {//Statement合并
 	}
 	else {
 		return 7;
+	}
+}
+int inWhichAOI3(int x, int y) {//Statement合并
+	if (x >= diagram_LU.x&&x <= diagram_RB.x &&y >= diagram_LU.y&&y <= diagram_RB.y) {
+		return 0;
+	}
+	else if (x >= option_LU.x&&x <= option_RB.x &&y >= option_LU.y&&y <= option_RB.y) {
+		return 1;
+	}
+	else if (x >= stament_LU.x&&x <= stament_RB.x &&y >= stament_LU.y&&y <= stament_RB.y) {
+		return 2;
+	}
+	else if (x >= time_LU.x&&x <= time_RB.x &&y >= time_LU.y&&y <= time_RB.y) {
+		return 3;
+	}
+	else {
+		return 4;
 	}
 }
 int my_split(const string& src, const char& delim,
@@ -1029,6 +1050,7 @@ int main()
 	system("pause");
 */
 
+/*
 	//Sequence7多文件 不分类型了
 	FILE *fp;
 	const string in_dir = "E:\\read-all\\";
@@ -1171,4 +1193,157 @@ int main()
 		out_file.close();
 	}
 	system("pause");
+*/
+
+	//Sequence8相比Sequence7 Option合并了
+	FILE *fp;
+	const string in_dir = "E:\\read-all\\";
+	const string out_dir = "E:\\out\\user\\";
+	//vector<string> names{//correct
+	//	"Project77-70 Recording18","Project77-70 Recording25","Project77-70 Recording26",
+	//	"Project77-70 Recording31","Project77-70 Recording46","Project77-70 Recording70",
+	//	"Project63-57 Recording23","Project63-57 Recording24","Project63-57 Recording28",
+	//	"Project63-57 Recording32","Project63-57 Recording63",
+	//	"Project77_56-49 Recording20","Project77_56-49 Recording27","Project77_56-49 Recording33",
+	//	"Project77_56-49 Recording35","Project77_56-49 Recording38",
+	//	"Project48-39 Recording19","Project48-39 Recording29","Project48-39 Recording34",
+	//	"Project48-39 Recording40","Project48-39 Recording45","Project48-39 Recording47",
+	//	"Project48-39 Recording49","Project48-39 Recording55",
+	//	
+	//	"Project33-24 Recording17","Project33-24 Recording36",
+	//	"Project33-24 Recording37","Project33-24 Recording39",
+	//	"Project33-24 Recording57","Project33-24 Recording59","Project33-24 Recording65",
+	//	"Project33-24 Recording66","Project33-24 Recording67",
+	//	"Project33-24 Recording72",
+	//	"Project22-1 Recording50",
+	//	"Project22-1 Recording51",
+	//	"Project22-1 Recording58","Project22-1 Recording61",
+	//	"Project22-1 Recording62","Project22-1 Recording64"
+	//};
+	
+	vector<string> names{//incorrect
+		"Project63-57 Recording30","Project48-39 Recording71","Project48-39 Recording52",
+		"Project33-24 Recording68","Project33-24 Recording41","Project33-24 Recording16",
+		"Project22-1 Recording56","Project22-1 Recording53","Project22-1 Recording42",
+		"Project22-1 Recording48","Project22-1 Recording54","Project22-1 Recording69"
+	};
+
+	//vector<string> names{
+	//	"Project77-70 Recording70"
+	//};
+
+	string in_path;
+	for (int k = 0; k < names.size(); k++) {
+		in_path = in_dir + names[k] + ".tsv";
+		cout << "第" << k << "个文件路径为：" << in_path << endl;
+
+		const string out_path = out_dir + names[k] + ".txt";
+		ofstream out_file(out_path, ofstream::out);
+
+		fp = fopen(in_path.c_str(), "r");//string to const char*
+		if (!fp)
+		{
+			cout << "OPEN ERROR!" << endl;
+			return 0;
+		}
+		int i = 0;
+		EyeMovementTypeTime eyeMovementTypeTime;//8 AOIs
+		vector<string> sequence;
+
+		char original_data[20000], data2[1000];
+		fgets(original_data, sizeof(original_data), fp);
+		const char * split = "\t";
+		string last_eye_movement_type = "0";
+		int start_flag = 0;
+		vector <string> res;//用于记录总数
+		while (fgets(original_data, sizeof(original_data), fp))
+		{
+			i++;//recode current line
+			vector<string> line;
+			//;get a line data in .tsv
+			my_split(original_data, '\t', line);
+			if (line.size() == 0) {
+				break;
+			}
+			// 11->54 :Eye movement type; 
+			// 12->55 Gaze event duration;
+			// 14,15->26,27 Fixation point X/Y->Gaze point X/Y;
+			// 14,15->57，58 Fixation point 
+			if (line[54] != last_eye_movement_type) {//line11 Eye movement type
+				//eyeMovementTypeTime.addTime(line[11], stoi(line[12]));
+				if (line[54] == "Fixation") {
+					//if (line[14] != ""&&line[15] != "") {//line 14,15 Fixation point X,Fixation point Y
+					int index = inWhichAOI3(atoi(line[57].c_str()), atoi(line[58].c_str()));
+					//ss << AOIs[index] << "," << line[12] << "->";//line 12 Gaze event duration
+					if (index == 4) {//Else不要了
+						continue;
+					}
+					string temp = AOIs3[index] + "," + line[55];// + "->"
+					res.push_back(temp);
+					//}
+				}
+				else if (line[54] == "Saccade") {
+					//ss << "Saccade" << "," << line[12] << "->";//line 12 Gaze event duration
+
+					int index = inWhichAOI3(atoi(line[26].c_str()), atoi(line[27].c_str()));
+					if (index == 4) {//Else不要了
+						continue;
+					}
+					string temp = AOIs3[index] + "," + line[55];// + "->"
+					res.push_back(temp);
+				}
+				else if (line[54] == "Unclassified") {
+
+					int index = inWhichAOI3(atoi(line[26].c_str()), atoi(line[27].c_str()));
+					if (index == 4) {//Else不要了
+						continue;
+					}
+					string temp = AOIs3[index] + "," + line[55];// + "->"
+					res.push_back(temp);
+				}
+
+			}
+			last_eye_movement_type = line[54];
+
+			if (feof(fp))
+				break;
+		}
+		string lastType = "0";
+		int flag = 0;//开始有重复
+		int type_num = 0, duration = 0;//type重复出现的次数
+		int last_duration = 0;
+		stringstream ss;
+
+		vector<pair<string, pair<int, int>>> merge_res;
+
+		for (int i = 0; i < res.size(); i++) {
+			string temp = res[i];
+			vector<string> type_duration;
+			my_split(temp, ',', type_duration);
+			if (merge_res.empty() || type_duration[0] != merge_res.back().first) {
+				pair<int, int> num_total(1, stoi(type_duration[1]));
+				pair<string, pair<int, int>> temp(type_duration[0], num_total);
+
+				merge_res.push_back(temp);
+			}
+			else if (type_duration[0] == merge_res.back().first) {
+				merge_res.back().second.second += stoi(type_duration[1]);
+				merge_res.back().second.first++;
+			}
+
+		}
+		for (pair<string, pair<int, int>> a : merge_res) {
+			ss << a.first << "," << a.second.first << "," << a.second.second << endl;
+		}
+		//ss << "total sequence " << merge_res.size() << endl;
+		//cout << ss.str() << endl;
+		out_file << ss.str() << endl;
+		cout << ss.str() << endl;
+		fclose(fp);
+
+		out_file.close();
+	}
+	system("pause");
+
+
 }
